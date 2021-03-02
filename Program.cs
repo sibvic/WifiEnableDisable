7890c1677462f1c850d5e1e8582f54912d25ae7b
Victor <sibvic@gmail.com>
2021-03-02 09:12:02 +0600

    feat: Initial version

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+using System;
+using System.Threading.Tasks;
+
+class Wifi
+{
+    public Wifi(WifiOptions options)
+    {
+        _options = options;
+    }
+
+    readonly WifiOptions _options;
+    Random _rnd = new Random();
+
+    public async Task<int> StartAsync()
+    {
+        while (true)
+        {
+            await Task.Delay(_options.Uptime * 60000);
+            Console.WriteLine("Disabling wifi");
+            System.Diagnostics.Process.Start("netsh", "interface set interface \"" + _options.Inteface + "\" disable");
+
+            var downtime = _rnd.Next(_options.DowntimeMin * 60, _options.DowntimeMax * 60);
+            await Task.Delay(downtime * 1000);
+            Console.WriteLine("Enabling wifi");
+            System.Diagnostics.Process.Start("netsh", "interface set interface \"" + _options.Inteface + "\" enable");
+        }
+        return 0;
+    }
+}
+
+namespace WifiEnableDisable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<WifiOptions>(args)
+                .MapResult(
+                    (WifiOptions opts) => new Wifi(opts).StartAsync().Result,
+                    errs => 1);
+        }
+    }
+}
---
+++ WifiOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+[Verb("wifi", HelpText = "Disable/enable wifi on timer.")]
+class WifiOptions
+{
+    [Option("uptime", HelpText = "How long to keep it enabled, minutes.", Default = 10)]
+    public int Uptime { get; set; }
+
+    [Option("downtime-min", HelpText = "How long to keep it disabled (min), minutes.", Default = 3)]
+    public int DowntimeMin { get; set; }
+
+    [Option("downtime-max", HelpText = "How long to keep it disabled (max), minutes.", Default = 7)]
+    public int DowntimeMax { get; set; }
+
+    [Option("interface", Required = true, HelpText = "Name of the interface")]
+    public string Inteface { get; set; }
+}
